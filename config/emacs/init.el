e34bc3004b9f9b31e3cdd606068538f4cc8e42e3
Lyndsey Dickson <lyndsey.dickson@l3harris.com>
2023-05-22 09:24:55 -0400

    Add git gutter to emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -12,12 +12,45 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
+;; Add redo to evil
+(evil-set-undo-system 'undo-redo)
+
+;; Download git-gutter+
+(unless (package-installed-p 'git-gutter+)
+  (package-install 'git-gutter+))
+
+;; Enable git-gutter+
+(global-git-gutter+-mode)
+
+;; git-gutter+ keybindings
+(global-set-key (kbd "C-x g") 'git-gutter+-mode) ; Turn on/off in the current buffer
+(global-set-key (kbd "C-x G") 'global-git-gutter+-mode) ; Turn on/off globally
+
+(eval-after-load 'git-gutter+
+'(progn
+    ;;; Jump between hunks
+    (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+    (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+
+    ;;; Act on hunks
+    (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+    (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+    ;; Stage hunk at point.
+    ;; If region is active, stage all hunk lines within the region.
+    (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+    (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+    (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)
+    (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+    (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(git-gutter+ evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
