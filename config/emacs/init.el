ee9a150d4ce8972530449457f0eb4c1dbcbf4985
Lyndsey Dickson <lyndsey.dickson@l3harris.com>
2023-06-27 17:53:18 -0400

    update kak and emacs configurations

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -5,6 +5,10 @@
 (package-initialize)
 (package-refresh-contents)
 
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -44,13 +48,63 @@
     (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
     (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)))
 
+;; Add fzf
+(use-package fzf
+  :ensure fzf
+  :bind
+    ;; Don't forget to set keybinds!
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        ;; fzf/grep-command "rg --no-heading -nH"
+        fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
+
+;; Auto generate tags
+(defun er-refresh-etags (&optional extension)
+    "Run etags on all peer files in current dir and reload them silently."
+    (interactive)
+    (shell-command (format "ctags -e *.%s" (or extension "el")))
+    (let ((tags-revert-without-query t))  ; don't query, revert silently          
+	(visit-tags-table default-directory nil)))
+
+(defadvice find-tag (around refresh-etags activate)
+"Rerun etags and reload tags if tag not found and redo find-tag.              
+If buffer is modified, ask about save before running etags."
+(let ((extension (file-name-extension (buffer-file-name))))
+(condition-case err
+ad-do-it
+    (error (and (buffer-modified-p)
+	(not (ding))
+	(y-or-n-p "Buffer is modified, save it? ")
+	(save-buffer))
+	(er-refresh-etags extension)
+	ad-do-it))))
+
+(use-package dumb-jump
+  :ensure dumb-jump
+  :config
+  (setq xref-show-definitions-function #'xref-show-definitions-completing-read))
+
+;; (use-package helm
+;;   :ensure helm
+;;   :config
+;;   (require 'helm-autoloads))
+
+(use-package lsp-mode
+  :ensure lsp-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(git-gutter+ evil)))
+ '(package-selected-packages '(dumb-jump fzf use-package git-gutter+ evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
